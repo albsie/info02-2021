2b35b921a1fe647e27e1e06f42f2482fe7803cff
Alber Sieghard <albsieweb@gmail.com>
2021-12-09 16:58:19 +0100

    feat: init cryptocoins

---
+++ exercises/cryptocoins/index.php
@@ -0,0 +1,6 @@
+<?php
+
+include_once "src/partials/head.php";
+include_once "src/partials/header.php";
+include_once "src/partials/main.php";
+include_once "src/partials/footer.php";
---
+++ exercises/cryptocoins/src/partials/header.php
@@ -0,0 +1,19 @@
+<header>
+  <nav>
+    <ul>
+      <li><a href="/"><img src="" alt="CryptoCoins"></a></li>
+      <li>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="aboutus">Über uns</a></li>
+        </ul>
+      </li>
+      <li>
+        <ul>
+          <li><a href="signin">Anmeldung</a></li>
+          <li><a href="signup">Registrieren</a></li>
+        </ul>
+      </li>
+    </ul>
+  </nav>
+</header>
