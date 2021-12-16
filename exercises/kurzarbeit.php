3b239b71f4a50ee33c79ad74cbc5ea74ec0a353a
Alber Sieghard <albsieweb@gmail.com>
2021-12-16 09:34:07 +0100

    feat: Kurzarbeit

---
+++ exercises/kurzarbeit.php
@@ -0,0 +1,43 @@
+<?php
+// 1. Erstelle das Array "names" und füge dem Array 5 Namen hinzu
+// 2. Füge dem Array "names" ein weiteren Namen mit PHP dazu
+// 3. Lösche im Array "names" den dritten und den fünften Eintrag mit PHP
+/*
+  4.  Gib das Array "names" im Browser aus und erhalte folgende Ausgabe mit genau dieser Struktur:
+      Der 1. Name ist [Name]
+      Der 2. Name ist [Name]
+      Der 3. Name ist [Name]
+      ...
+*/
+// 5. Sortiere das Array alphabetisch
+/*
+  6. Schreibe eine Begrüßung:
+  Wenn die Variable $greeting den Wert "Herr" hat, soll "Sehr geehrter Herr" ausgegeben werden
+  Wenn die Variable $greeting den Wert "Frau" hat, soll "Sehr geehrte Frau" ausgegeben werden
+  Wenn die Variable $greeting keinen Wert hat, soll "Sehr geehrte Damen und Herren" ausgegeben werden
+*/
+
+
+// 7. Schreibe eine Funktion, wo ein Namen als Parameter übergeben wird. Dabei soll folgende  Ausgabe erziehlt werden: 
+// Hallo [Wert des Parameters]
+
+/* 8. Programmiere eine Schleife um im Browser folgende Ausgabe zu erhalten: 
+*
+**
+***
+****
+*****
+******
+*******
+********
+*********
+**********
+*/
+
+// 9. Erstelle im HTML ein Inputfeld mit einem Button. Immer wenn man auf Speichern klickt soll das Inputfeld zum vorhergehenden Wert addiert werden. Ausgabe unter dem Input Feld.
+
+
+// Jede Antwort bringt 10 Punkte außer 8. und 9.Frage bringen 15 Punkte - wenn alle 9 Fragen richtig beantwortet sind gibt es die Note 1
+
+
+ ?>
