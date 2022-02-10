67e3b819156ccfd0fcdc5448f23ec6b1651d0249
Alber Sieghard <albsieweb@gmail.com>
2022-02-10 14:00:56 +0100

    feat: update:kurzarbeit

--- exercises/kurzarbeit.php
+++ exercises/kurzarbeit.php
@@ -1,53 +1,49 @@
 <?php
-// 1. Erstelle das Array "names" und füge dem Array 5 Namen hinzu
-// 2. Füge dem Array "names" ein weiteren Namen mit PHP dazu
-// 3. Lösche im Array "names" den dritten und den fünften Eintrag mit PHP
+// 1. Erstelle das Array "days" und füge dem Array 5 Tage hinzu
+// 2. Füge dem Array "days" ein weiteren Tag mit PHP dazu
+// 3. Lösche im Array "days" den ersten und den letzten Eintrag mit PHP
 /*
-  4.  Gib das Array "names" im Browser aus und erhalte folgende Ausgabe mit genau dieser Struktur:
-      Der 1. Name ist [Name]
-      Der 2. Name ist [Name]
-      Der 3. Name ist [Name]
+  4.  Gib das Array "days" im Browser aus und erhalte folgende Ausgabe mit genau dieser Struktur:
+      Der 1. Tag ist [day]
+      Der 2. Tag ist [day]
+      Der 3. Tag ist [day]
       ...
 */
-// 5. Sortiere das Array alphabetisch
+// 5. Sortiere das Array alphabetisch in umgekehrter Reihenfolge.
 /*
-  6. Schreibe eine Begrüßung:
+  6. Schreibe eine Begrüßung mit Kontrollstrukturen:
   Wenn die Variable $greeting den Wert "Herr" hat, soll "Sehr geehrter Herr" ausgegeben werden
   Wenn die Variable $greeting den Wert "Frau" hat, soll "Sehr geehrte Frau" ausgegeben werden
   Wenn die Variable $greeting keinen Wert hat, soll "Sehr geehrte Damen und Herren" ausgegeben werden
 */
 
 
-// 7. Schreibe eine Funktion, wo ein Namen als Parameter übergeben wird. Dabei soll folgende  Ausgabe erziehlt werden:
-// Hallo [Wert des Parameters]
+// 7. Schreibe eine Funktion, wo ein Name [$name] und eine Anrede [$title] als Parameter übergeben werden. Dabei soll folgender Returnwert beim aufruf der Funktion erziehlt werden:
+// Hallo [$title] [$name] -> Hallo Herr Mair
 
 /* 8. Programmiere eine Schleife um im Browser folgende Ausgabe zu erhalten:
-*
-**
-***
-****
-*****
-******
-*******
-********
-*********
-**********
+1
+12
+123
+12345
+123456
+1234567
+12345678
+123456789
 */
 
 // 9. Erstelle im HTML ein Inputfeld mit einem Button. Immer wenn man auf Speichern klickt soll das Inputfeld zum vorhergehenden Wert addiert werden. Ausgabe unter dem Input Feld.
+// Tipp: gib das Ergebnis vom POST Request immer wieder mit, dann kannst du es dazu addieren.
 
 
 
 
 // Jede Antwort bringt 10 Punkte außer 8. und 9.Frage bringen 15 Punkte - wenn alle 9 Fragen richtig beantwortet sind gibt es die Note 1
-$number = intval($_POST['btn']);
-if (isset($_POST['int'])) {
-    $number += intval($_POST['int']);
-}
+
 
  ?>
- <form method="post">
-   <input type="text" name="int">
-   <button type="submit" name="btn" value="<?=$number?>">Addieren</button>
-   <div><?=$number?></div>
+ <form>
+   <input type="text">
+   <button type="submit">">Addieren</button>
+   <div>----->Ergebnis Ausgabe hier<-----</div>
  </form>
