5977581bccaa92b322c7d1349fc508608da1f7f9
Alber Sieghard <albsieweb@gmail.com>
2022-02-17 16:06:21 +0100

    fix: db.php

--- exercises/cryptocoins/config/db.php
+++ exercises/cryptocoins/config/db.php
@@ -4,5 +4,6 @@ $host = "localhost";
 $user = "root";
 $password = "";
 $database = "cryptocoins";
+$irgendwas = "sfsd";
 
 $db = new PDO('mysql:host=' . $host . ';dbname=' . $database, $user, $password);
